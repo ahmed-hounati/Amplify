ca274d26e57047677ab27c9928c023e3cdc9082a
ahmed-hounati <hounatiahmed@gmail.com>
2024-05-14 13:28:52 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://g9hc7scfph.execute-api.eu-north-1.amazonaws.com/dev/media")
+      .then(res => {
+        console.log(res.data)
+        setUsers(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []); // empty dependency array to run effect only once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users.map((user, index) => (
+          <div key={index}>
+            <h1>{user.name}</h1>
+            <ul>
+              {user.users.map((nestedUser, nestedIndex) => (
+                <li key={nestedIndex}>{nestedUser.name}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+      }
     </div>
   );
 }
